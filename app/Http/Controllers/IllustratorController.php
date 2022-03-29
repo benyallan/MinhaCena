58d4a4c0467ac4120207b0dff51ea40054d185fa
Beny Allan <benyallan@yahoo.com.br>
2022-03-29 18:19:59 -0300

    Modelos criados

---
+++ app/Http/Controllers/IllustratorController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Illustrator;
+use Illuminate\Http\Request;
+
+class IllustratorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Illustrator  $illustrator
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Illustrator $illustrator)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Illustrator  $illustrator
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Illustrator $illustrator)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Illustrator  $illustrator
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Illustrator $illustrator)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RedactionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Redaction;
+use Illuminate\Http\Request;
+
+class RedactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Redaction  $redaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Redaction $redaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Redaction  $redaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Redaction $redaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Redaction  $redaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Redaction $redaction)
+    {
+        //
+    }
+}
